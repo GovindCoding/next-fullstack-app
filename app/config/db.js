a7c78bd505f43652510597eb2c514f4f9c6c7688
Govind Ekshinge <govind.ekshinge@anka.co.in>
2021-06-14 14:08:37 +0530

    Adding mysql changes by Govind

--- app/config/db.js
+++ app/config/db.js
@@ -1,12 +1,20 @@
 'use strict'
 const mysql = require("mysql");
-const dbConfig = require("./db.config.js");
+// const dbConfig = require("./db.config.js");
+
+// var connection = mysql.createConnection({
+//     host: dbConfig.HOST,
+//     user: dbConfig.USER,
+//     password: dbConfig.PASSWORD,
+//     database: dbConfig.DB,
+// });
 
 var connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB,
+    host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+    user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+    port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+    database : process.env.OPENSHIFT_APP_NAME
 });
 
 connection.connect(function(err) {
