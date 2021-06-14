26b0dd07068a8181e9878847c482f955fd1e753a
Govind Ekshinge <govind.ekshinge@anka.co.in>
2021-06-14 15:41:02 +0530

    Adding db changes by Govind

--- app/config/db.js
+++ app/config/db.js
@@ -11,11 +11,11 @@ const mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
-    user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
-    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
     port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
-    database : process.env.OPENSHIFT_APP_NAME
-});
+    user     : 'db_user',
+    password : 'Anka@1234',
+    database : 'node_app'
+    });
 
 connection.connect(function(err) {
     if (err) {
